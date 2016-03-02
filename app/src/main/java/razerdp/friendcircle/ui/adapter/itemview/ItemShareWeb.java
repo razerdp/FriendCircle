705ecdab52c9e682eb1185e6b0fc85beff05d85f
razerdp <razerdp123@gmail.com>
2016-03-02 15:41:07 +0800

    // 网页分享

--- app/src/main/java/razerdp/friendcircle/ui/adapter/itemview/ItemShareWeb.java
+++ app/src/main/java/razerdp/friendcircle/ui/adapter/itemview/ItemShareWeb.java
@@ -2,9 +2,11 @@ package razerdp.friendcircle.ui.adapter.itemview;
 
 import android.support.annotation.NonNull;
 import android.view.View;
+import android.widget.TextView;
 import razerdp.friendcircle.R;
 import razerdp.friendcircle.api.adapter.view.BaseItemDelegate;
 import razerdp.friendcircle.api.data.entity.MomentsInfo;
+import razerdp.friendcircle.widget.SuperImageView;
 
 /**
  * Created by 大灯泡 on 2016/2/25.
@@ -12,13 +14,10 @@ import razerdp.friendcircle.api.data.entity.MomentsInfo;
  * type=11
  */
 public class ItemShareWeb extends BaseItemDelegate {
+    private SuperImageView webThumb;
+    private TextView webTitle;
 
     public ItemShareWeb(){}
-
-    @Override
-    protected void bindData(int position, @NonNull View v, @NonNull MomentsInfo data, int dynamicType) {
-
-    }
 
     @Override
     public int getViewRes() {
@@ -27,6 +26,13 @@ public class ItemShareWeb extends BaseItemDelegate {
 
     @Override
     public void onFindView(@NonNull View parent) {
+        if (webThumb==null)webThumb= (SuperImageView) parent.findViewById(R.id.web_thumb);
+        if (webTitle==null)webTitle= (TextView) parent.findViewById(R.id.web_title);
+    }
 
+    @Override
+    protected void bindData(int position, @NonNull View v, @NonNull MomentsInfo data, int dynamicType) {
+        webThumb.loadImageDefault(data.content.webImg);
+        webTitle.setText(data.content.webTitle);
     }
 }
