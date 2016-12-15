e6c2cee1dbdee2da85c7b1f863d4fc0c7e6dd97b
razerdp <razerdp123@gmail.com>
2016-12-15 17:06:10 +0800

    // 更新包，优化forceClickImageView控件

--- app/src/main/java/razerdp/friendcircle/ui/widget/imageview/ForceClickImageView.java
+++ app/src/main/java/razerdp/friendcircle/ui/widget/imageview/ForceClickImageView.java
@@ -18,6 +18,7 @@ import razerdp.friendcircle.R;
  * 朋友圈的imageview，包含点击动作
  */
 public class ForceClickImageView extends ImageView {
+    private static final String TAG = "ForceClickImageView";
     //前景层
     private Drawable mForegroundDrawable;
     private Rect mCachedBounds = new Rect();
@@ -32,48 +33,52 @@ public class ForceClickImageView extends ImageView {
 
     public ForceClickImageView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context, attrs);
+        init(context, attrs, true);
+        setFocusable(true);
     }
 
     /**
      * 初始化
      */
-    private void init(Context context, AttributeSet attrs) {
+    private void init(Context context, AttributeSet attrs, boolean needDefaultForceGroundColor) {
         final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ForceClickImageView);
         mForegroundDrawable = a.getDrawable(R.styleable.ForceClickImageView_foregroundColor);
-        if (mForegroundDrawable instanceof ColorDrawable) {
-            int foreGroundColor = a.getColor(R.styleable.ForceClickImageView_foregroundColor, 0x55c6c6c6);
+        if (mForegroundDrawable instanceof ColorDrawable || (attrs == null && needDefaultForceGroundColor)) {
+            int foreGroundColor = a.getColor(R.styleable.ForceClickImageView_foregroundColor, 0x882b2b2b);
             mForegroundDrawable = new StateListDrawable();
             ColorDrawable forceDrawable = new ColorDrawable(foreGroundColor);
             ColorDrawable normalDrawable = new ColorDrawable(Color.TRANSPARENT);
-            ((StateListDrawable) mForegroundDrawable).addState(new int[] { android.R.attr.state_focused },
-                    forceDrawable);
-            ((StateListDrawable) mForegroundDrawable).addState(new int[] { android.R.attr.state_pressed },
-                    forceDrawable);
-            ((StateListDrawable) mForegroundDrawable).addState(new int[] { android.R.attr.state_enabled },
-                    normalDrawable);
-            ((StateListDrawable) mForegroundDrawable).addState(new int[] {}, normalDrawable);
+            ((StateListDrawable) mForegroundDrawable).addState(new int[]{android.R.attr.state_pressed}, forceDrawable);
+            ((StateListDrawable) mForegroundDrawable).addState(new int[]{android.R.attr.state_focused}, forceDrawable);
+            ((StateListDrawable) mForegroundDrawable).addState(new int[]{android.R.attr.state_enabled}, normalDrawable);
+            ((StateListDrawable) mForegroundDrawable).addState(new int[]{}, normalDrawable);
         }
-        if (mForegroundDrawable != null) mForegroundDrawable.setCallback(this);
+        if (mForegroundDrawable != null) {
+            mForegroundDrawable.setCallback(this);
+        }
         a.recycle();
     }
+
 
     @Override
     protected void drawableStateChanged() {
         super.drawableStateChanged();
-
         if (mForegroundDrawable != null && mForegroundDrawable.isStateful()) {
             mForegroundDrawable.setState(getDrawableState());
+            invalidate();
         }
-        invalidate();
     }
+
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
         if (mForegroundDrawable != null) {
-            mForegroundDrawable.setBounds(mCachedBounds);
+            if (getDrawable()!=null) {
+                mForegroundDrawable.setBounds(getDrawable().getBounds());
+            }else {
+                mForegroundDrawable.setBounds(mCachedBounds);
+            }
             mForegroundDrawable.draw(canvas);
         }
     }
@@ -81,7 +86,7 @@ public class ForceClickImageView extends ImageView {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-
         if (mForegroundDrawable != null) mCachedBounds.set(0, 0, w, h);
     }
+
 }
