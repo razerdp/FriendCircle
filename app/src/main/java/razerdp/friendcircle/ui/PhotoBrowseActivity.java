74e8a033d86f7abefe13fb50ae0514310dfbdc7a
razerdp <razerdp123@gmail.com>
2017-02-09 11:58:22 +0800

    // 增加图片浏览指示器，优化部分体验

--- app/src/main/java/razerdp/friendcircle/ui/PhotoBrowseActivity.java
+++ app/src/main/java/razerdp/friendcircle/ui/PhotoBrowseActivity.java
@@ -49,8 +49,6 @@ public class PhotoBrowseActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_photo_browse);
         preInitData();
         initView();
